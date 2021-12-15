efd06921e6df255bed7a7c9110f03837d042e349
MuhammadDhiyaulAuliya <59078451+MuhammadDhiyaul@users.noreply.github.com>
2021-12-15 22:55:23 +0700

    Validation of Form

--- resources/views/pizza/create.blade.php
+++ resources/views/pizza/create.blade.php
@@ -9,7 +9,7 @@
                 <div class="card-body">
                     <ul class="list-group">
                         <a href="" class="list-group-item list-group-item-action">View</a>
-                        <a href="" class="list-group-item list-group-item-action">Add</a>
+                        <a href="" class="list-group-item list-group-item-action">Create</a>
 
                     </ul>
                 </div>
@@ -19,7 +19,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Pizza</div>
-
+            <form action="{{route('pizza.store')}}" method="POST"> @csrf
                 <div class="card-body">
                     <div class="form-group">
                         <label for="name">Name of Pizza</label>
@@ -31,13 +31,13 @@
                     </div>
                     <div class="form-inline">
                         <label>Pizza Price ($)</label>
-                        <input type="number" class="form-control" placeholder="small pizza price">
-                        <input type="number" class="form-control" placeholder="medium pizza price">
-                        <input type="number" class="form-control" placeholder="large pizza price">
+                        <input type="number" name="small_pizza_price" class="form-control" placeholder="small pizza price">
+                        <input type="number" name="medium_pizza_price" class="form-control" placeholder="medium pizza price">
+                        <input type="number" name="large_pizza_price" class="form-control" placeholder="large pizza price">
                     </div>
                     <div class="form-group">
                         <label for="description">Category</label>
-                        <select class="form-control">
+                        <select class="form-control" name="category">
                             <option value=""></option>
                             <option value="vegetarian">Vegetarian Pizza</option>
                             <option value="nonvegetarian">Non Vegetarian Pizza</option>
@@ -46,12 +46,13 @@
                     </div>
                     <div class="form-group">
                         <label>Image</label>
-                        <input type="file" class="form-control" name="image">
+                        <input type="file" name="image" class="form-control" name="image">
                     </div>
                     <div class="form-group text-center">
                         <button class="btn btn-primary" type="submit">Save</button>
                     </div>
                 </div>
+            </form>
             </div>
         </div>
     </div>
